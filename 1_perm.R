f91acf2a796786df5223ea9442ee812405298f80
kendomaniac <raffaele.calogero@gmail.com>
2018-03-19 11:46:14 +0100

    creating casc project

--- 1_perm.R
+++
@@ -1,9 +0,0 @@
- 
-args <- commandArgs(TRUE)
-if(length(args)==7){
-system(paste("./1_perm.sh",args[1]," ",args[2]," ",args[3]," ",args[4]," ",args[5]," ",args[6]," ",args[7] ))
-}else{ system("./1_perm.sh")}
-
-
-
-
