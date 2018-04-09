47ee5676ff08f1026681156afeeded1406ba0393
kendomaniac <raffaele.calogero@gmail.com>
2018-04-09 12:50:50 -0400

    preparing vignette

---
+++ R/genesUmi.R
@@ -0,0 +1,29 @@
+#' @title Plotting genes to UMIs relationship.
+#' @description This function create a plot in which genes are plotted with respect to total counts UMI for each cell.
+#' @param data.folder, a character string indicating the folder where output will be saved.
+#' @param counts.matrix, a character string indicating the the name of tab delimited file  of cells un-normalized expression counts.
+#' @param umiXgene, a integer defining how many UMI are required to call a gene as present. default: 3
+#' @return pdf with the cells counts distributions: genes.umi.pdf
+#'
+#' @export
+genesUmi <- function(data.folder=getwd(), counts.matrix, umiXgene=3){
+
+  #running time 1
+  ptm <- proc.time()
+  #running time 1
+  test <- dockerTest()
+  if(!test){
+    cat("\nERROR: Docker seems not to be installed in your system\n")
+    return()
+  }
+  setwd(data.folder)
+  tmp <- read.table(counts.matrix, sep="\t", heder=T, row.names=1, stringsAsFactors = F)
+  genes <- apply(tmp,2, function(x, umiXgene){
+     x[which(x <  umiXgene)] <- 0
+     x[which(x >=  umiXgene)] <- 1
+  })
+  umi <- apply(tmp,2, sum)
+  pdf("genes.umi.pdf")
+     plot(log10(umi), genes, xlab="log10 UMI", ylab="# of genes")
+  dev.off()
+}
