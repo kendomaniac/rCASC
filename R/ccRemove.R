ac78b3f140e03f37d3137da6a405132e356cd2b3
alessandriLuca <alessandri.luca1991@gmail.com>
2018-06-12 13:11:07 +0200

    bug correction

--- R/ccRemove.R
+++ R/ccRemove.R
@@ -19,7 +19,7 @@ ccRemove <- function(group=c("sudo","docker"), scratch.folder,file,separator,see
 
 data.folder=dirname(file)
 positions=length(strsplit(basename(file),"\\.")[[1]])
-matrixNameC=strsplit(basename(a),"\\.")[[1]]
+matrixNameC=strsplit(basename(file),"\\.")[[1]]
 matrixName=paste(matrixNameC[seq(1,positions-1)],collapse="")
 format=strsplit(basename(basename(file)),"\\.")[[1]][positions]
 
