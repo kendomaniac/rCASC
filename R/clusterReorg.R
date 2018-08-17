3faba335968690771af1fe80b0b4c601af855216
alessandriLuca <alessandri.luca1991@gmail.com>
2018-08-17 17:49:54 +0200

    Update clusterReorg.R

--- R/clusterReorg.R
+++ R/clusterReorg.R
@@ -2,12 +2,10 @@
 #' @description This function executes a ubuntu docker that merge two clusters
 #' @param group, a character string. Two options: sudo or docker, depending to which group the user belongs
 #' @param scratch.folder, a character string indicating the path of the scratch folder
-#' @param data.folder, a character string indicating the folder where input data are located and where output will be written
-#' @param matrixName, counts table name. Matrix data file must be in data.folder. The file MUST contain RAW counts, without any modification, such as log transformation, normalizatio etc.
+#' @param file, a character string indicating the path of the file, with file name and extension included
 #' @param nCluster, number of Cluster used in Kmeans to generate the clusters that you want to merge
 #' @param A, first Cluster that has to be merged
 #' @param B, second Cluster that has to be merged
-#' @param format, matrix count format, "csv", "txt"#' @param B, second Cluster that has to be merged
 #' @param separator, separator used in count file, e.g. '\\t', ','
 #' @param sp, minimun number of percentage of cells that has to be in common between two permutation to be the same cluster.
 #' @author Luca Alessandri , alessandri [dot] luca1991 [at] gmail [dot] com, University of Torino
