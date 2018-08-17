528038f110ea4b3a78a934357320947f59e0a2f3
alessandriLuca <alessandri.luca1991@gmail.com>
2018-08-17 17:56:28 +0200

    Update permutationClustering.R

--- R/permutationClustering.R
+++ R/permutationClustering.R
@@ -2,14 +2,12 @@
 #' @description This function executes a ubuntu docker that produces a specific number of permutation to evaluate clustering.
 #' @param group, a character string. Two options: sudo or docker, depending to which group the user belongs
 #' @param scratch.folder, a character string indicating the path of the scratch folder
-#' @param data.folder, a character string indicating the folder where input data are located and where output will be written
-#' @param matrixName, counts table name. Matrix data file must be in data.folder. The file MUST contain RAW counts, without any modification, such as log transformation, normalizatio etc.
+#' @param file, a character string indicating the path of the file, with file name and extension included
 #' @param nPerm, number of permutations to perform the pValue to evaluate clustering
 #' @param permAtTime, number of permutations that can be computes in parallel
 #' @param percent, percentage of random cells that has to be removed in each permutation
 #' @param range1, first number of cluster for k means algorithm
 #' @param range2, last number of cluster for k means algorithm
-#' @param format, count matrix format "csv", "txt"..
 #' @param separator, separator used in count file, e.g. '\\t', ','
 #' @param logTen, 1 if the count matrix is already in log10, 0 otherwise
 #' @param clustering, clustering method to use : "SIMLR" , "tSne", "griph"
