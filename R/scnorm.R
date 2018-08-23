e205ba2470811c961c77dc1b874f9abf8ef8ca7c
kendomaniac <raffaele.calogero@gmail.com>
2018-08-23 11:26:21 +0200

    update

--- R/scnorm.R
+++ R/scnorm.R
@@ -1,8 +1,7 @@
 #' @title Running SCnorm  normalization
 #' @description This function is a wrapper for SCnorm: robust normalization of single-cell RNA-seq data (Bacher et al. Nature Methods 2017, 14:584–586)
 #' @param group, a character string. Two options: sudo or docker, depending to which group the user belongs
-#' @param data.folder, a character string indicating the folder where comma separated file of cells log10 counts is saved
-#' @param counts.matrix, a character string indicating the the name of tab delimited file  file of cells un-normalized expression counts
+#' @param file, a character string indicating the path of the file. IMPORTANT: full path to the file MUST be included. Only tab delimited files are supported
 #' @param conditions, vector of condition labels, this should correspond to the columns of the un-normalized expression matrix. If not provided data is assumed to come from same condition/batch.
 #' @param outputName, specify the path and/or name of output files.
 #' @param nCores, number of cores to use, default is detectCores() - 1.
@@ -18,13 +17,26 @@
 #'     system("wget http://130.192.119.59/public/example_UMI.txt.zip")
 #'     unzip("example_UMI.txt.zip")
 #'     conditions=rep(1,12)
-#'     scnorm(group="docker", data.folder=getwd(),counts.matrix="example_UMI.txt",
+#'     scnorm(group="docker", file=paste(getwd(), "example_UMI.txt", sep="/"),
 #'     conditions=conditions,outputName="example_UMI", nCores=8, filtercellNum=10,
 #'     ditherCount=TRUE, PropToUse=0.1, PrintProgressPlots=FALSE, FilterExpression=1)
 
 #' }
 #' @export
-scnorm <- function(group=c("sudo","docker"), data.folder=getwd(), counts.matrix, conditions=NULL, outputName, nCores=8, filtercellNum = 10, ditherCount=FALSE, PropToUse=0.1, PrintProgressPlots=FALSE, FilterExpression=0){
+scnorm <- function(group=c("sudo","docker"), file, conditions=NULL, outputName, nCores=8, filtercellNum = 10, ditherCount=FALSE, PropToUse=0.1, PrintProgressPlots=FALSE, FilterExpression=0){
+
+  data.folder=dirname(file)
+
+  positions=length(strsplit(basename(file),"\\.")[[1]])
+  matrixNameC=strsplit(basename(file),"\\.")[[1]]
+  matrixName=paste(matrixNameC[seq(1,positions-1)],collapse="")
+  format=strsplit(basename(basename(file)),"\\.")[[1]][positions]
+  if(format=="txt"){
+    counts.matrix <- paste(matrixName, format, sep=".")
+  }else{
+    cat("\nOnly tab delimited files with extention txt are supported\n")
+    return("Not a tab delimited file")
+  }
 
   #running time 1
   ptm <- proc.time()
@@ -34,6 +46,7 @@ scnorm <- function(group=c("sudo","docker"), data.folder=getwd(), counts.matrix,
     cat("\nERROR: Docker seems not to be installed in your system\n")
     return()
   }
+  home <- setwd()
   setwd(data.folder)
   if(is.null(conditions)){
     cat("\nERROR: Conditions are missing\n")
@@ -99,5 +112,6 @@ scnorm <- function(group=c("sudo","docker"), data.folder=getwd(), counts.matrix,
   system(paste("cp ",paste(path.package(package="casc"),"containers/containers.txt",sep="/")," ",data.folder, sep=""))
 
   system(paste("docker rm ", container.id, sep=""))
+  setwd(home)
 
 }
