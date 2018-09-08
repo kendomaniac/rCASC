31a8260ae479dcb753f4a827120fde36172b6ad0
kendomaniac <raffaele.calogero@gmail.com>
2018-09-08 12:12:49 +0200

    adding tsneBootstrap

--- R/tsneBootstrap.R
+++ R/tsneBootstrap.R
@@ -13,6 +13,7 @@
 #' @param seed, important value to reproduce the same results with same input
 #' @param sp, minimun number of percentage of cells that has to be in common between two permutation to be the same cluster, default 0.8.
 #' @param clusterPermErr, error that can be done by each permutation in cluster number depicting.Default = 0.05
+#' @param perplexity, Number of close neighbors for each point. This parameter is specific for tSne. Default value is 10. Setting this parameter when use a clustering method different by tSne will be ignored.
 
 
 #' @author Luca Alessandri, alessandri [dot] luca1991 [at] gmail [dot] com, University of Torino
@@ -23,14 +24,14 @@
 #' system("wget http://130.192.119.59/public/section4.1_examples.zip")
 #' unzip("section4.1_examples.zip")
 #' setwd("section4.1_examples")
-#' simlrBootstrap(group="docker",scratch.folder="/data/scratch/",file=paste(getwd(), "bmsnkn_5x100cells.txt", sep="/"), nPerm=160, permAtTime=8, percent=10, range1=4, range2=6, separator="\t",logTen=0, seed=111, sp=0.8, clusterPermErr=0.05)
+#' tsneBootstrap(group="docker",scratch.folder="/data/scratch/",file=paste(getwd(), "bmsnkn_5x100cells.txt", sep="/"), nPerm=160, permAtTime=8, percent=10, range1=4, range2=6, separator="\t",logTen=0, seed=111, sp=0.8, clusterPermErr=0.05, perplexity=10)
 #'}
 #' @export
 
-simlrBootstrap <- function(group=c("sudo","docker"), scratch.folder, file, nPerm, permAtTime, percent, range1, range2, separator, logTen=0, seed=111, rK=0, sp=0.8, clusterPermErr=0.05, maxDeltaConfidence=NULL, minLogMean=NULL){
+tsneBootstrap <- function(group=c("sudo","docker"), scratch.folder, file, nPerm, permAtTime, percent, range1, range2, separator, logTen=0, seed=111, sp=0.8, clusterPermErr=0.05, perplexity=10){
 
-  permutationClustering(group=group, scratch.folder=scratch.folder, file=file, nPerm=nPerm, permAtTime=permAtTime, percent=percent, range1=range1, range2=range2, separator=separator, logTen=logTen, clustering="SIMLR", perplexity=10 , seed=seed, rK=0)
-  permAnalysis(group=group, scratch.folder=scratch.folder,file=file, range1=range1, range2=range2, separator=separator, sp=sp, clusterPermErr=clusterPermErr, maxDeltaConfidence=0.01, minLogMean=0.05)
+  permutationClustering(group=group, scratch.folder=scratch.folder, file=file, nPerm=nPerm, permAtTime=permAtTime, percent=percent, range1=range1, range2=range2, separator=separator, logTen=logTen, clustering="tSne", perplexity=10 , seed=seed, rK=0)
+  permAnalysis(group=group, scratch.folder=scratch.folder,file=file, range1=range1, range2=range2, separator=separator, sp=sp, clusterPermErr=clusterPermErr, maxDeltaConfidence=maxDeltaConfidence=0.01, minLogMean=minLogMean=0.05)
 
 }
 
