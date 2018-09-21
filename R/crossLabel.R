e6172c011df9c434f4347be20d786ea04c3a9c6d
alessandriLuca <alessandri.luca1991@gmail.com>
2018-09-21 11:18:44 +0200

    Added controls

--- R/crossLabel.R
+++ R/crossLabel.R
@@ -76,6 +76,10 @@ separator="tab"
   }
     if (!file.exists(paste(data.folder,"/Results/",matrixName,"_Label.",format,sep=""))){
     cat(paste("\nSeems that in Results folder there is no Label file!\n"))
+      cat(paste("\Label file has to be named as follow :",matrixName,"_Label.",format," \n",sep=""))
+          cat(paste("\n Label file need to be in ",data.folder,"/Results/"," \n",sep=""))
+
+
     system("echo 3 > ExitStatusFile 2>&1")
     setwd(data.folder)
     return(3)
