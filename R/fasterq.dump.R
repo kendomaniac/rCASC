5bc7b628d0c3db9d33a6b0a83010e4f8cd00f537
kendomaniac <raffaele.calogero@gmail.com>
2018-12-26 13:57:18 +0100

    update

--- R/fasterq.dump.R
+++ R/fasterq.dump.R
@@ -77,7 +77,7 @@ sraDownload <- function(group=c("sudo","docker"), sra.name, data.folder, scratch
   
   #saving log and removing docker container
   container.id <- readLines(paste(data.folder,"/dockerID", sep=""), warn = FALSE)
-  system(paste("docker logs ", substr(container.id,1,12), " &> ",data.folder,"/fastqc_", substr(container.id,1,12),".log", sep=""))
+  system(paste("docker logs ", substr(container.id,1,12), " &> ",data.folder,"/sraDownload_", substr(container.id,1,12),".log", sep=""))
   system(paste("docker rm ", container.id, sep=""))
   
   cat("\n\nRemoving the temporary file ....\n")
