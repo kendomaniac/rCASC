d83faa539cf9fe57d523cd51aa7ba7217b30b810
kendomaniac <raffaele.calogero@gmail.com>
2020-05-21 13:10:52 +0200

    adding spatialAnalysis2 function

---
+++ R/spatialAnalysisPlus.R
@@ -0,0 +1,137 @@
+#' @title spatial Analysis Plus
+#' @description This function analyzes the data that came up from permutationClustering script with spatial transcriptomics data. The function integrates CSS with a score, which is related to the beloging cluster identity of a cell with respect to the other 6 cell neighborhood that surround it.
+#' @param group, a character string. Two options: sudo or docker, depending to which group the user belongs
+#' @param scratch.folder, a character string indicating the path of the scratch folder
+#' @param file, a character string indicating the path of the file, with file name and extension included
+#' @param nCluster, Last number of cluster that has to be analyzed
+#' @param separator, separator used in count file, e.g. '\\t', ','
+#' @param tissuePosition, file with tissue position name with extension
+#' @param Sp, Threshold to assign the plus score. 1 mean that all 6 cells, sorrounding the cell of interest, belong to the same cluster of the cell of interest. 0.83 out mean 5 out of 6 are of the same cluster of the reference cell, 0.66 mean 4 out of 6. We do not suggest to go below these three options.  
+#' @param percentageIncrease, percentage of the CSS score that has to be increased if the Threshold condition is satisfied. 
+
+#' @author Luca Alessandri , alessandri [dot] luca1991 [at] gmail [dot] com, University of Torino
+#'
+#' @return stability plot for each nCluster,two files with score information for each cell for each permutation.
+#' @examples
+#'\dontrun{
+#'spatialAnalysis2(group=c("sudo"), scratch.folder="/home/lucastormreig/temp/", file="/home/lucastormreig/test/spatialAnalysis/spatial/HLNrevised.csv",nCluster=5,separator=",",tissuePosition="/home/lucastormreig/test/spatialAnalysis/spatial/Results/tissue_positions_list.csv",Sp=0.8,percentageIncrease=10)
+
+#'}
+#' @export
+spatialAnalysis2 <- function(group=c("sudo","docker"), scratch.folder, file,nCluster,separator,tissuePosition,Sp=0.8,percentageIncrease=10){
+
+  data.folder=dirname(file)
+positions=length(strsplit(basename(file),"\\.")[[1]])
+matrixNameC=strsplit(basename(file),"\\.")[[1]]
+matrixName=paste(matrixNameC[seq(1,positions-1)],collapse="")
+format=strsplit(basename(basename(file)),"\\.")[[1]][positions]
+
+  #running time 1
+  ptm <- proc.time()
+  #setting the data.folder as working folder
+  if (!file.exists(data.folder)){
+    cat(paste("\nIt seems that the ",data.folder, " folder does not exist\n"))
+    return(2)
+  }
+
+  #storing the position of the home folder
+  home <- getwd()
+  setwd(data.folder)
+  #initialize status
+  system("echo 0 > ExitStatusFile 2>&1")
+
+  #testing if docker is running
+  test <- dockerTest()
+  if(!test){
+    cat("\nERROR: Docker seems not to be installed in your system\n")
+    system("echo 10 > ExitStatusFile 2>&1")
+    setwd(home)
+    return(10)
+  }
+
+
+
+  #check  if scratch folder exist
+  if (!file.exists(scratch.folder)){
+    cat(paste("\nIt seems that the ",scratch.folder, " folder does not exist\n"))
+    system("echo 3 > ExitStatusFile 2>&1")
+    setwd(data.folder)
+    return(3)
+  }
+  tmp.folder <- gsub(":","-",gsub(" ","-",date()))
+  scrat_tmp.folder=file.path(scratch.folder, tmp.folder)
+  writeLines(scrat_tmp.folder,paste(data.folder,"/tempFolderID", sep=""))
+  cat("\ncreating a folder in scratch folder\n")
+  dir.create(file.path(scrat_tmp.folder))
+  #preprocess matrix and copying files
+
+
+if(separator=="\t"){
+separator="tab"
+}
+   
+system(paste("cp -r ",data.folder,"/Results/* ",scrat_tmp.folder,sep=""))
+system(paste("cp ",data.folder,"/",matrixName,".",format," ",scrat_tmp.folder,sep=""))
+system(paste("cp ",tissuePosition," ",scrat_tmp.folder,sep=""))
+
+
+
+
+
+data.folderT=dirname(tissuePosition)
+positionsT=length(strsplit(basename(tissuePosition),"\\.")[[1]])
+matrixNameCT=strsplit(basename(tissuePosition),"\\.")[[1]]
+matrixNameT=paste(matrixNameCT[seq(1,positionsT-1)],collapse="")
+formatT=strsplit(basename(basename(tissuePosition)),"\\.")[[1]][positionsT]
+
+
+
+
+  #executing the docker job
+    params <- paste("--cidfile ",data.folder,"/dockerID -v ",scrat_tmp.folder,":/scratch -v ", data.folder, ":/data -d repbioinfo/spatialanalysis2 Rscript /home/extraScore.R ",matrixName," ",nCluster," ",format," ",separator," ",matrixNameT," ",Sp," ",percentageIncrease,sep="")
+
+resultRun <- runDocker(group=group, params=params)
+
+  #waiting for the end of the container work
+  if(resultRun==0){
+  #  system(paste("cp ", scrat_tmp.folder, "/* ", data.folder, sep=""))
+  }
+  #running time 2
+  ptm <- proc.time() - ptm
+  dir <- dir(data.folder)
+  dir <- dir[grep("run.info",dir)]
+  if(length(dir)>0){
+    con <- file("run.info", "r")
+    tmp.run <- readLines(con)
+    close(con)
+    tmp.run[length(tmp.run)+1] <- paste("user run time mins ",ptm[1]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("system run time mins ",ptm[2]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("elapsed run time mins ",ptm[3]/60, sep="")
+    writeLines(tmp.run,"run.info")
+  }else{
+    tmp.run <- NULL
+    tmp.run[1] <- paste("run time mins ",ptm[1]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("system run time mins ",ptm[2]/60, sep="")
+    tmp.run[length(tmp.run)+1] <- paste("elapsed run time mins ",ptm[3]/60, sep="")
+
+    writeLines(tmp.run,"run.info")
+  }
+
+  #saving log and removing docker container
+  container.id <- readLines(paste(data.folder,"/dockerID", sep=""), warn = FALSE)
+  system(paste("docker logs ", substr(container.id,1,12), " &> ",data.folder,"/", substr(container.id,1,12),".log", sep=""))
+  system(paste("docker rm ", container.id, sep=""))
+
+
+  #Copy result folder
+ cat("Copying Result Folder")
+  system(paste("cp -r ",scrat_tmp.folder,"/* ",data.folder,"/Results",sep=""))
+  #removing temporary folder
+  cat("\n\nRemoving the temporary file ....\n")
+  system(paste("rm -R ",scrat_tmp.folder))
+  system("rm -fR out.info")
+  system("rm -fR dockerID")
+  system("rm  -fR tempFolderID")
+  system(paste("cp ",paste(path.package(package="rCASC"),"containers/containers.txt",sep="/")," ",data.folder, sep=""))
+  setwd(home)
+}
