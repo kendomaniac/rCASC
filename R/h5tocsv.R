1a9f03732643587b4611fdb99c249873a2d09ec3
Raffaele Calogero <raffaele.calogero@gmail.com>
2024-05-17 06:02:39 +0200

    updating h5tocsv

--- R/h5tocsv.R
+++ R/h5tocsv.R
@@ -36,6 +36,8 @@ h5tocsv <- function(group=c("sudo","docker"),  file, type=c("h5","10xgenomics"),
     dockerImage="docker.io/repbioinfo/cellranger.2018.03"
   } else if(version == "5"){
     dockerImage="docker.io/repbioinfo/cellranger.2020.05"
+  } else if(version == "7"){
+    dockerImage="docker.io/repbioinfo/repbioinfo/cellranger.2023.7.1.0"
   }
   
   
